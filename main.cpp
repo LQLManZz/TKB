04b85cb6f7c4b93e2f9a4d9ab9464dbea37e1b72
Long <long55dbp@gmail.com>
2024-12-30 19:26:44 +0700

    BT slide 93

--- main.cpp
+++ main.cpp
@@ -40,18 +40,18 @@ using namespace std;
 class tinh
 {
 public:
-    tinh(int a = 0, int b = 0)
+    // tinh(int a = 0, int b = 0)
+    // {
+    //     this->a = a;
+    //     this->b = b;
+    // }
+    friend istream &operator>>(istream &is, tinh &t)
     {
-        tinh::a = a;
-        tinh::b = b;
-    }
-    friend istream &operator>>(istream &is, tinh t)
-    {
-        cout << "Nhap 2 so a,b :";
+        cout << "Nhap 2 so a,b: ";
         is >> t.a >> t.b;
         return is;
     }
-    friend ostream &operator<<(ostream &os, tinh p)
+    friend ostream &operator<<(ostream &os, const tinh &p)
     {
         os << "Tong 2 so la: " << p.a + p.b;
         os << "\nHieu 2 so la: " << p.a - p.b;
@@ -62,10 +62,44 @@ private:
     int a, b;
 };
 
+class BT
+{
+public:
+    friend istream &operator>>(istream &in, BT &a)
+    {
+        do
+        {
+            cout << "Nhap so nguyen x: ";
+            in >> a.x;
+        } while (a.x > 10);
+        return in;
+    }
+    friend ostream &operator<<(ostream &out, BT &a)
+    {
+        a.y = 2 * a.x; // Initialize y before using it
+        for (int l = 0; l < 20; l++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                a.A[l][j] = l * float(a.x) - 2 * j / float(a.y);
+                out << "A[" << l << "][" << j << "] = " << a.A[l][j] << endl;
+            }
+        }
+        return out;
+    }
+
+private:
+    float A[20][10];
+    int x, y;
+};
+
 int main()
 {
-    tinh s1;
-    cin >> s1;
-    cout << s1;
+    // tinh s1;
+    // cin >> s1;
+    // cout << s1;
+    BT test1;
+    cin >> test1;
+    cout << test1;
     return 0;
 }
