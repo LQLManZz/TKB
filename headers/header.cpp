c168cd710195a17c0be56a9d43ac6dd017a9b03d
Long <long55dbp@gmail.com>
2025-02-26 00:10:13 +0700

    task0 fix3

--- headers/header.cpp
+++ headers/header.cpp
@@ -5,3 +5,4 @@
 #include <string>
 #include <sstream>
 #include <cctype>
+#include <algorithm>
--- phamletricong.cpp
+++ phamletricong.cpp
@@ -11,9 +11,75 @@ private:
     int LF1[17], LF2[17], EXP1, EXP2, T1, T2, E;
     string data[MAX_LINES][MAX_LINE_LENGTH];
     string filename, line;
+    string *pData;
 
 public:
     task0(const string &filename) : filename(filename) {}
+
+    // ham thuc hien task 0
+    bool readFile()
+    {
+        fstream file(filename, ios::in);
+
+        // check xem file co ton tai ko
+        if (!file)
+        {
+            return false;
+        }
+
+        // check xem file co noi dung ko
+        if (file.peek() == ifstream::traits_type::eof())
+        {
+            return false;
+        }
+
+        // check xem noi dung file co hop le ko
+        if (!checkIfFileValid(filename))
+        {
+            return false;
+        }
+
+        // read file roi dua vao mang 2D
+        int i = 0;
+        pData = (string *)data;
+        while (getline(file, line) && i < MAX_LINES)
+        {
+            spaceFilter(line);
+            stringstream ss(line);
+            string num;
+            int j = 0;
+            while (ss >> num && j < MAX_LINE_LENGTH)
+            {
+                *(pData + i * MAX_LINE_LENGTH + j) = num;
+                j++;
+            }
+            i++;
+        }
+
+        // insert data vao cac bien
+        insertData(LF1, pData, 0, 0, 1000);
+        insertData(LF2, pData, 1, 0, 1000);
+        insertData(EXP1, EXP2, pData, 2, 0, 600);
+        insertData(T1, T2, pData, 3, 0, 3000);
+        insertData(E, pData, 4, 0, 99);
+
+        // printArray(pData);
+        // cout << endl;
+        // printArray(LF1);
+        // cout << endl;
+        // printArray(LF2);
+        // cout << endl;
+        // cout << EXP1 << endl;
+        // cout << EXP2 << endl;
+        // cout << T1 << endl;
+        // cout << T2 << endl;
+        // cout << E << endl;
+
+        file.close();
+
+        return true;
+    }
+
     // thay cac ki tu thanh space
     void spaceFilter(string &line)
     {
@@ -25,50 +91,71 @@ public:
             }
         }
     }
-    // in mang 2D ra man hinh
-    void printArray()
+
+    // check xem noi dung file co hop le ko
+    bool checkIfFileValid(const string &filename)
+    {
+        fstream file(filename, ios::in);
+        string lineTemp;
+        int elem;
+        int count = 0;
+        while (getline(file, lineTemp))
+        {
+            spaceFilter(lineTemp);
+            if (lineTemp.empty())
+                break;
+            stringstream ss1(lineTemp);
+            while (ss1 >> elem)
+            {
+                count++;
+            }
+            count++;
+        }
+        if (count < 44)
+            return false;
+        return true;
+    }
+
+    // in mang ra man hinh
+    void printArray(int *pLF)
+    {
+        for (int i = 0; i < MAX_LINE_LENGTH; i++)
+        {
+            cout << *(pLF + i) << " ";
+        }
+    }
+    void printArray(string *pData)
     {
         for (int i = 0; i < MAX_LINES; i++)
         {
             for (int j = 0; j < MAX_LINE_LENGTH; j++)
             {
-                cout << data[i][j] << " ";
+                cout << *(pData + i * MAX_LINE_LENGTH + j) << " ";
             }
             cout << endl;
         }
     }
-    bool readFile()
-    {
-        fstream file(filename, ios::in);
-        // check xem file co ton tai ko
-        if (!file)
-        {
-            return false;
-        }
-        // check xem file co noi dung ko
-        if (file.peek() == ifstream::traits_type::eof())
-        {
-            return false;
-        }
-        // read file roi dua vao mang 2D
-        int i = 0;
-        while (getline(file, line) && i < MAX_LINES)
-        {
-            spaceFilter(line);
-            stringstream ss(line);
-            string num;
-            int j = 0;
-            while (ss >> num && j < MAX_LINE_LENGTH)
-            {
-                data[i][j] = num;
-                j++;
-            }
-            i++;
-        }
-        file.close();
 
-        printArray();
-        return true;
+    // insert data vao cac bien
+    void insertData(int *pLF, string *pData, const int &row, const int &min, const int &max)
+    {
+        for (int i = 0; i < MAX_LINE_LENGTH; i++)
+        {
+            *(pLF + i) = stoi(*(pData + row * MAX_LINE_LENGTH + i));
+            *(pLF + i) = clamp(*(pLF + i), min, max);
+        }
+    }
+    void insertData(int &A, int &B, string *pData, const int &row, const int &min, const int &max)
+    {
+        A = stoi(*(pData + row * MAX_LINE_LENGTH));
+        A = clamp(A, min, max);
+        B = stoi(*(pData + row * MAX_LINE_LENGTH + 1));
+        B = clamp(B, min, max);
+    }
+    void insertData(int &C, string *pData, const int &row, const int &min, const int &max)
+    {
+        C = stoi(*(pData + row * MAX_LINE_LENGTH));
+        C = clamp(C, min, max);
     }
 };
 
@@ -81,6 +168,6 @@ public:
 int main()
 {
     task0 t1("inputfile.txt");
-    t1.readFile();
+    cout << t1.readFile();
     return 0;
 }
